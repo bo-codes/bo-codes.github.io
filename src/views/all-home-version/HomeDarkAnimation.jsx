aab2151e7682fe6e1e6b2a85d3d5456be7ad26f3
elias <99310710+bo-codes@users.noreply.github.com>
2022-08-28 23:11:49 -0700

    fixing name

--- src/views/all-home-version/HomeDarkAnimation.jsx
+++ src/views/all-home-version/HomeDarkAnimation.jsx
@@ -12,7 +12,7 @@ import Footer from "../../components/footer/FooterAnimation";
 import useDocumentTitle from "../../components/useDocumentTitle";
 
 const HomeFour = () => {
-  useDocumentTitle("Home Dark Animation || React Personal Portfolio Template");
+  useDocumentTitle("Bo-codes");
   return (
     <div className="home-dark">
       <Header />
